1ddcff1a7bd0de85777761989fbb710947d16f4c
AnastasiyaK <stasykorchagina@yandex.ru>
2022-11-24 16:28:03 +0500

    feat: Спринт 14. исправить комментарии по ревью 2

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,11 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.marker.Marker;
-import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
